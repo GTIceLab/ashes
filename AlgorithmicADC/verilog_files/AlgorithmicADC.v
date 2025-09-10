75d7cfc6f07043144e1fdc2d8883dcc72a51b9e2
prenziv <prenziv@gmail.com>
2025-09-09 21:08:47 -0400

    Some algebraic ADC routing

--- AlgorithmicADC/verilog_files/AlgorithmicADC.v
+++ AlgorithmicADC/verilog_files/AlgorithmicADC.v
@@ -2,30 +2,43 @@ module TOP(port1);
 
 
 	/* Island 0 */
-	EPOT I__0 (.island_num(0), .row(0), .col(0), .matrix_row(1), .matrix_col(1));
-	EPOT I__1 (.island_num(0), .row(1), .col(0), .matrix_row(1), .matrix_col(1));
-	EPOT I__2 (.island_num(0), .row(2), .col(0), .matrix_row(1), .matrix_col(1));
-	AnalogBuffer I__3 (.island_num(0), .row(7), .col(0), .matrix_row(1), .matrix_col(1));
-	TSMC350nm_Amplifier9T_FGBias I__4 (.island_num(0), .row(4), .col(0), .matrix_row(1), .matrix_col(1));
-	TSMC350nm_Amplifier9T_FGBias I__5 (.island_num(0), .row(5), .col(0), .matrix_row(1), .matrix_col(1));
+	EPOT I__0 (.island_num(0), .row(0), .col(0), .matrix_row(1), .matrix_col(1), .Progrow_0(net191), .VD_P_0_row_0(net200[0]), .VD_P_1_row_0(net201[0]));
+	EPOT I__1 (.island_num(0), .row(1), .col(0), .matrix_row(1), .matrix_col(1), .VD_P_0_row_0(net202[0]), .VD_P_1_row_0(net203[0]));
+	EPOT I__2 (.island_num(0), .row(2), .col(0), .matrix_row(1), .matrix_col(1), .VD_P_0_row_0(net204[0]), .VD_P_1_row_0(net205[0]));
 
  	/*Programming Mux */ 
-	TSMC350nm_VinjDecode2to4_htile decoder(.island_num(0), .direction(horizontal), .bits(2));
-	TSMC350nm_IndirectSwitches switch(.island_num(0), .direction(horizontal), .num(1));
-	TSMC350nm_VinjDecode2to4_vtile decoder(.island_num(0), .direction(vertical), .bits(4));
-	TSMC350nm_drainSelect_progrundrains switch(.island_num(0), .direction(vertical), .num(4), .type(drain_select));
-	TSMC350nm_4TGate_ST_draincutoff switch(.island_num(0), .direction(vertical), .num(4), .type(prog_switch));
+	TSMC350nm_VinjDecode2to4_htile decoder(.island_num(0), .direction(horizontal), .bits(2), .decode_n0_ENABLE(net197), .decode_n0_n0_IN_1_(net199), .decode_n0_n0_IN_0_(net198));
+	TSMC350nm_IndirectSwitches switch(.island_num(0), .direction(horizontal), .num(1), .switch_n0_PROG(net191), .switch_n0_RUN(net190));
+	TSMC350nm_VinjDecode2to4_vtile decoder(.island_num(0), .direction(vertical), .bits(4), .decode_n0_IN_1_(net195), .decode_n0_IN_0_(net194), .decode_n2_IN_1_(net193), .decode_n2_IN_0_(net192), .decode_n0_ENABLE(net196));
+	TSMC350nm_drainSelect_progrundrains switch(.island_num(0), .direction(vertical), .num(3), .type(drain_select));
+	TSMC350nm_4TGate_ST_draincutoff switch(.island_num(0), .direction(vertical), .num(3), .type(prog_switch), .switch_n0_PR_0_(net200[0]), .switch_n0_PR_1_(net201[0]), .switch_n0_PR_2_(net202[0]), .switch_n0_PR_3_(net203[0]), .switch_n1_PR_0_(net204[0]), .switch_n1_PR_1_(net205[0]), .switch_n1_PR_2_(net206[0]), .switch_n1_PR_3_(net208[0]), .switch_n2_PR_0_(net210[0]), .switch_n1_In_2_(net207[0]), .switch_n1_In_3_(net209[0]), .switch_n0_RUN(net190));
 
 
 	/* Island 1 */
-	TGate_DT I__0 (.island_num(1), .row(0), .col(0), .matrix_row(6), .matrix_col(1));
+	AnalogBuffer I__0 (.island_num(1), .row(0), .col(0), .matrix_row(1), .matrix_col(1), .Vd_Prow_0(net210[0]), .Vinrow_0(net111[0]), .Voutrow_0(net114[1]));
 
  	/*Programming Mux */ 
 
 
 	/* Island 2 */
-	Capacitor_80ff I__0 (.island_num(2), .row(0), .col(0), .matrix_row(5), .matrix_col(1));
+	TSMC350nm_Amplifier9T_FGBias I__0 (.island_num(2), .row(0), .col(0), .matrix_row(1), .matrix_col(1), .VD_Prow_0(net206[0]), .VD_Rrow_0(net207[0]), .PROGrow_0(net191));
+	TSMC350nm_Amplifier9T_FGBias I__1 (.island_num(2), .row(1), .col(0), .matrix_row(1), .matrix_col(1), .VD_Prow_0(net208[0]), .VD_Rrow_0(net209[0]));
 
  	/*Programming Mux */ 
 
+
+	/* Island 3 */
+	TGate_DT I__0 (.island_num(3), .row(0), .col(0), .matrix_row(6), .matrix_col(1), .SELAcol_0(net108[0:6]), .Ccol_0(net111[0:6]), .Acol_0(net114[0:6]));
+
+ 	/*Programming Mux */ 
+
+
+	/* Island 4 */
+	Capacitor_80ff I__0 (.island_num(4), .row(0), .col(0), .matrix_row(5), .matrix_col(1), .Topcol_0(net123[0:5]), .Botcol_0(net124[0:5]));
+
+ 	/*Programming Mux */ 
+
+
+	/* Frame */ 
+	tile_analog_frame cab_frame(.pin_layer(METAL3), .N_n_PROG(net191), .N_n_RUN(net190), .W_w_DrainB_0_(net192), .W_w_DrainB_1_(net193), .W_w_DrainB_2_(net194), .W_w_DrainB_3_(net195), .W_w_DrainEnable(net196), .W_w_GateB_0_(net198), .W_w_GateB_1_(net199), .W_w_GateEnable(net197), .W_w_VIN(net114[0]), .E_e_CLK_Sample(net108[0]), .E_e_CLK_Amp(net108[1]), .E_e_CLK_RST(net108[2]));
  endmodule
--- example_python/AlgorithmicADC.py
+++ example_python/AlgorithmicADC.py
@@ -31,44 +31,134 @@ Vreset.markAbut()
 Vreset.place([2,0])
 
 # Buffers out Sample and Hold
-Buffer = lib_dc.TSMC350nm_AnalogBuffer(Top,ADCIsland)
+ADCIsland2 = ac.Island(Top)
+Buffer = lib_dc.TSMC350nm_AnalogBuffer(Top,ADCIsland2)
 Buffer.markAbut()
-Buffer.place([7,0])
+Buffer.place([0,0])
 
 
 # Open-loop TA-based comparator
-Comparator = lib_dc.TSMC350nm_Amplifier9T_FGBias(Top,ADCIsland) 
-Comparator.place([4,0])
+ADCIsland3 = ac.Island(Top)
+Comparator = lib_dc.TSMC350nm_Amplifier9T_FGBias(Top,ADCIsland3) 
+Comparator.place([0,0])
 Comparator.markAbut()
 
 # Inverting Amplifier
-InvAmp = lib_dc.TSMC350nm_Amplifier9T_FGBias(Top,ADCIsland) 
-InvAmp.place([5,0])
+InvAmp = lib_dc.TSMC350nm_Amplifier9T_FGBias(Top,ADCIsland3)
+InvAmp.place([1,0])
 InvAmp.markAbut()
 
 TgateIsland = ac.Island(Top)
 Tgates = lib_dc.TSMC350nm_TGate_DT(Top,TgateIsland,dim=[6,1])
 Tgates.place([0,0])
 
+
 CapIsland = ac.Island(Top)
 Caps = lib_dc.TSMC350nm_Capacitor_80ff(Top,CapIsland,dim=[5,1])
 Caps.place([0,0])
 
+# FG Programming
+# ----------------------------------------------------------------------
+
 GateDecoder = lib_mux.STD_IndirectGateDecoder(Top,ADCIsland,2)
 GateSwitches = lib_mux.STD_IndirectGateSwitch(Top,ADCIsland,1)
 
-drainLineNum = 13
+drainLineNum = 9 
 drainBits = int(np.ceil(np.log2(drainLineNum)))
 
 DrainDecoder = lib_mux.STD_DrainDecoder(Top,ADCIsland,bits=drainBits)
 DrainSelect = lib_mux.RunDrainSwitch(Top,ADCIsland,num=int(np.ceil(drainLineNum/4)))
 DrainSwitch = lib_cab.DrainCutoff(Top,ADCIsland,num=int(np.ceil(drainLineNum/4)))
 
-EPOTWidth = 85000
-DecoderWidth = int(43000 + ((drainBits/2)*25000))+15000
 
-location_islands= ( (0,0) , (DecoderWidth+EPOTWidth+5000,0) , (DecoderWidth+EPOTWidth+20000,0) )
+# Pins
+# ---------------------------------------------------------------------
+outerPins = lib_mux.frame(Top)
+
+PROG = outerPins.createPort("N","PROG")
+RUN = outerPins.createPort("N","RUN")
+
+
+DrainBits = outerPins.createPort("W","DrainB",dimension=drainBits)
+DrainEnable = outerPins.createPort("W","DrainEnable")
+GateBits = outerPins.createPort("W","GateB",dimension=2)
+GateEnable = outerPins.createPort("W","GateEnable")
+
+VIN = outerPins.createPort("W","VIN")
+CLK_Sample = outerPins.createPort("E","CLK_Sample")
+CLK_Amp = outerPins.createPort("E","CLK_Amp")
+CLK_RST = outerPins.createPort("E","CLK_RST")
+
+
+# Pin Connections
+# --------------------------------------------------------------------
+GateSwitches.PROG += PROG
+GateSwitches.RUN += RUN
+DrainSwitch.RUN += RUN
+Comparator.PROG += PROG
+CompThreshold.Prog += PROG
+
+DrainDecoder.IN += DrainBits
+DrainDecoder.ENABLE += DrainEnable
+
+GateDecoder.ENABLE += GateEnable
+GateDecoder.IN += GateBits
+
+CompThreshold.VD_P += DrainSwitch.PR[0:2]
+CapSwitch0.VD_P += DrainSwitch.PR[2:4]
+Vreset.VD_P += DrainSwitch.PR[4:6]
+
+Comparator.VD_P += DrainSwitch.PR[6]
+Comparator.VD_R += DrainSwitch.In[6]
+
+InvAmp.VD_P += DrainSwitch.PR[7]
+InvAmp.VD_R += DrainSwitch.In[7]
+
+Buffer.Vd_P += DrainSwitch.PR[8]
+
+VIN += Tgates.A[0]
+Buffer.Vin += Tgates.C[0]
+CLK_Sample += Tgates.SELA[0]
+
+CinTop = ac.Wire(Top)
+Caps.Top[0] += CinTop
+Caps.Top[1] += CinTop
+CinBot = ac.Wire(Top)
+Caps.Bot[0] += CinBot
+Caps.Bot[1] += CinBot
+
+Buffer.Vout += Tgates.A[1]
+Tgates.C[1] += CinTop
+Tgates.SELA[1] += CLK_Amp
+
+Tgates.A[2] += CinTop
+Tgates.C[2] += CinBot
+Tgates.SELA[2] += CLK_RST
+
+CsubTop = ac.Wire(Top)
+Caps.Top[2] += CsubTop 
+Caps.Top[3] += CsubTop 
+CsubTop += CinBot
+CsubBot = ac.Wire(Top)
+Caps.Bot[2] += CsubBot
+Caps.Bot[3] += CsubBot 
+
+CsubTop += InvAmp.VIN_MINUS
+
+
+
+
+
+# Placement
+# ------------------------------------------------------------------
+EPOTWidth = 85000
+Pitch = 22000
+DecoderWidth = int(43000 + ((drainBits/2)*25000))+15000
+xoffset = 5000
+yoffset = 0
+
+location_islands= ( (xoffset,4*Pitch) , (xoffset+DecoderWidth-4000,yoffset) , (xoffset+DecoderWidth-4000,1.5*Pitch), (xoffset+DecoderWidth+EPOTWidth+5000,yoffset), (xoffset+DecoderWidth+EPOTWidth+20000,yoffset) )
 
 design_limits = [5e5, 6e5]
 
-ac.compile_asic(Top,process="TSMC350nm",fileName="AlgorithmicADC",design_limits = design_limits, location_islands = location_islands, drainSpaceIdx=0,drainSpace=15,gateSpaceIdx=0,gateSpace=15)
+ac.compile_asic(Top,process="TSMC350nm",fileName="AlgorithmicADC",design_limits = design_limits, location_islands = location_islands, drainSpaceIdx=0,drainSpace=15,gateSpaceIdx=0,gateSpace=10)
