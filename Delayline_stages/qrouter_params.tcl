a9ecd5a5d488fc3c88af90d7ecf2a3ca21582e8b
ubuntu <ubuntu@ors-ashes.myguest.virtualbox.org>
2025-09-24 13:36:02 -0400

    Deleted folders of intermediate synthesis and all my versions of AvergerDAC

--- Delayline_stages/qrouter_params.tcl
+++
@@ -1,9 +0,0 @@
-read_lef Delayline_stages/Delayline_stages.lef
-read_def Delayline_stages/Delayline_stages.def
-cost via 90
-layers 4
-stage1 mask none
-stage2 mask none limit 100
-stage3 mask none
-write_def Delayline_stages/Delayline_stages_qroute.def
-quit
