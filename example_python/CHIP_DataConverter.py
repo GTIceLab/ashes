7208d35eafc3d1adcef64712771a311071fb0392
prenziv <prenziv@gmail.com>
2025-09-29 00:43:32 -0400

    Full routing python + cleanup

--- example_python/CHIP_DataConverter.py
+++ example_python/CHIP_DataConverter.py
@@ -111,6 +111,7 @@ macro.DVDD += chipframe.DVDD_N[2]
 
 # Supporting Circuitry
 # ---------------------------------------------------------------------
+# Debug Scanner
 DebugScannerIsland = ac.Island(Top)
 DebugScanner = lib_new.TSMC350nm_VerticalScanner(Top,DebugScannerIsland,dim=(4,1))
 DebugScanner.place([0,0])
@@ -122,11 +123,11 @@ DebugScanner.CLK += macro.mmio_reg_9_bout[13]
 DebugScanner.RSTBar += macro.mmio_reg_9_bout[14]
 DebugScanner.Din += macro.mmio_reg_9_bout[15]
 
-
+# Drain Decoder
 DrainSelIsland = ac.Island(Top)
-DrainDecoder = lib_mux.STD_DrainDecoder(Top,DrainSelIsland,bits=4)
-DrainSelect = lib_mux.RunDrainSwitch(Top,DrainSelIsland,num=4)
-DrainSwitch = lib_cab.DrainCutoff(Top,DrainSelIsland,num=4)
+DrainDecoder = lib_mux.STD_DrainDecoder(Top,DrainSelIsland,bits=3)
+DrainSelect = lib_mux.RunDrainSwitch(Top,DrainSelIsland,num=2)
+DrainSwitch = lib_cab.DrainCutoff(Top,DrainSelIsland,num=2)
 DrainSelLocation = (5000e3,250e3)
 FakeCell1=lib_new.FakeCellGateDecoder(Top,DrainSelIsland)
 FakeCell1.place([0,0])
@@ -134,13 +135,21 @@ FakeCell1.place([0,0])
 DrainSwitch.VDD += chipframe.VINJ_S[2]
 DrainSwitch.GND += chipframe.gnd_S[2]
 
-
+# Analog Buffers
 BufferIsland = ac.Island(Top)
 Buffers = lib_dc.TSMC350nm_AnalogBuffer(Top,BufferIsland,dim=(4,1))
 Buffers.place([0,0])
 
 GateDecoder = lib_mux.STD_IndirectGateDecoder(Top,BufferIsland,2)
 GateSwitches = lib_mux.STD_IndirectGateSwitch(Top,BufferIsland,1)
+GateDecoder.IN += macro.mmio_reg_5_vinj[0:2]
+GateDecoder.ENABLE += macro.mmio_reg_5_vinj[2]
+GateSwitches.RUN += macro.RUN_HV
+GateSwitches.PROG += macro.PROG_HV
+GateSwitches.GND += chipframe.gnd_S[2]
+GateSwitches.VDD += chipframe.VINJ_S[2]
+GateSwitches.Vgsel += macro.VGPROG
+GateSwitches.RUN_IN += macro.VGRUN[0]
 
 
 BufferLocation = (3250e3,250e3)
@@ -150,7 +159,13 @@ Buffers.GND_b += chipframe.gnd_S[2]
 Buffers.VINJ_b += chipframe.VINJ_S[2]
 Buffers.VINJ += GateSwitches.VINJ
 Buffers.Vg += GateSwitches.Vg[0]
+Buffers.Vsel += GateSwitches.CTRL_B[0]
 
+Buffers.Vd_P += DrainSwitch.PR[4:8]
+
+Buffers.Vout += chipframe.IO_S[14:18] 
+
+# Level Shifters
 LVLIsland = ac.Island(Top)
 LVLShift = TSMC350nm_LVLShift_x16(Top,LVLIsland)
 LVLShift.place([0,0])
@@ -160,6 +175,15 @@ LVLShift.DVDD += chipframe.DVDD_S[2]
 LVLShift.VINJ += chipframe.VINJ_S[2]
 LVLShift.GND += chipframe.gnd_S[2]
 
+for i in range(7):
+    LVLShift.Vin[i] += macro.mmio_reg_9_bout[5+i]
+
+
+# Extra Pins
+DebugScanner.Out += Buffers.Vin[0]
+DrainDecoder.IN += LVLShift.OUT[0:3]
+DrainSelect.prog_drainrail += macro.SystemDrainline[0]
+DrainSelect.run_drainrail += macro.SystemDrainline[1]
 
 # Data Converters
 #----------------------------------------------------------------------
@@ -216,7 +240,7 @@ RampADC.CLK += macro.mmio_reg_9_bout[0]
 RampADC.RST += macro.mmio_reg_10_bout[15]
 RampADC.Code += macro.mmio_reg_10_bout[5:13]
 
-RampADC.Vin += chipframe.IO_S[0]
+RampADC.Vin += chipframe.IO_S[12]
 
 RampADC.DEBUG += DebugScanner.In[5:7]
 RampADC.Drainline_Prog += DrainSwitch.PR[1]
@@ -247,7 +271,7 @@ AlgorithmicADC.CLK_RST += macro.mmio_reg_9_bout[2]
 AlgorithmicADC.CLK_Load += macro.mmio_reg_9_bout[3]
 AlgorithmicADC.CLK_Amp += macro.mmio_reg_9_bout[4]
 
-AlgorithmicADC.Vin += chipframe.IO_S[1]
+AlgorithmicADC.Vin += chipframe.IO_S[13]
 AlgorithmicADC.Code+= macro.mmio_reg_10_bout[13]
 
 AlgorithmicADC.DEBUG += DebugScanner.In[7:10]
