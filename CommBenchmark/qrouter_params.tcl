9c3e22cf632a8a6badc7abe72cd3900d2cb7e9b5
ubuntu <ubuntu@ors-ashes.myguest.virtualbox.org>
2025-10-09 15:57:30 -0400

    deleted top level individual routingg folders

--- CommBenchmark/qrouter_params.tcl
+++
@@ -1,9 +0,0 @@
-read_lef CommBenchmark/CommBenchmark.lef
-read_def CommBenchmark/CommBenchmark.def
-cost via 90
-layers 4
-stage1 mask none
-stage2 mask none limit 100
-stage3 mask none
-write_def CommBenchmark/CommBenchmark_qroute.def
-quit
