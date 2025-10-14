99ba53804b6412bedeb33baa7ddeb7de390d19f8
ubuntu <ubuntu@ors-ashes.myguest.virtualbox.org>
2025-10-14 13:24:06 -0400

    beamform sub algorithm python code

--- example_python/Delaylines.py
+++ example_python/Delaylines.py
@@ -5,6 +5,9 @@ from ashes_fg.class_lib_mux import *
 from ashes_fg.class_lib_cab import *
 from ashes_fg.asic.asic_systems import *
 
+import json
+import numpy as np
+
 Top = Circuit()
 
 # Placement
@@ -14,7 +17,7 @@ DrainDecoder = True
 V_NW=None
 
 # Delay Line Sizing
-rows = 32
+rows = 40
 columns = 9
 
 FakeIsland=Island(Top)
@@ -109,8 +112,31 @@ for i in range(columns):
         pointer += 1
 
 Gate_Route_Del = Island(Top)
-GR_Del = Gate_Routing_NoVGRUN(Top,dim=(1,int((columns+1)/2)),island=Gate_Route_Del)
+GR_Del = Gate_Routing_NoVGRUN(Top,dim=(1,int((columns+1)/2)-1),island=Gate_Route_Del)
 GR_Del.place([0,0])
+
+Gate_Route_Half = Island(Top)
+GR_Half = Gate_Routing_Half(Top,dim=(1,1),island=Gate_Route_Half)
+GR_Half.place([0,0])
+
+Scanner_Island = Island(Top)
+Scanner = [0]*3
+for i in range(3):
+    Scanner[i] = "Scanner_"+str(i)
+for i in range(3):
+    Scanner[i] = TSMC350nm_VerticalScanner(Top, Scanner_Island)
+    Scanner[i].place([i,0])
+
+DelayLine_instances[0][rows-1].V_NW += Scanner[0].In[0]
+DelayLine_instances[0][rows-1].V_NE += Scanner[0].In[1]
+DelayLine_instances[1][rows-1].V_NE += Scanner[0].In[2]
+DelayLine_instances[2][rows-1].V_NE += Scanner[0].In[3]
+DelayLine_instances[3][rows-1].V_NE += Scanner[1].In[0]
+DelayLine_instances[4][rows-1].V_NE += Scanner[1].In[1]
+DelayLine_instances[5][rows-1].V_NE += Scanner[1].In[2]
+DelayLine_instances[6][rows-1].V_NE += Scanner[1].In[3]
+DelayLine_instances[7][rows-1].V_NE += Scanner[2].In[0]
+DelayLine_instances[8][rows-1].V_NE += Scanner[2].In[1]
 
 outerPins = frame(Top)
 # General
@@ -129,15 +155,29 @@ GateB = outerPins.createPort("N","GateB",dimension=gateBits_Del)
 DrainB = outerPins.createPort("W","DrainB",dimension=drainBits_Del)
 DrainlineP = outerPins.createPort("W","Drainline_Prog")
 DrainlineR = outerPins.createPort("W","Drainline_Run")
+Input = outerPins.createPort("W","Input",dimension=rows)
+OUTS = outerPins.createPort("N","OUTS",dimension=(rows*columns)+rows)
+
+Scan_out = outerPins.createPort("S","WTA_out")
+Din = outerPins.createPort("S","Din")
+CLK = outerPins.createPort("S","CLK")
+RSTBar = outerPins.createPort("S","RSTBar")
 # Delay Lines
 # --------------------------------------------------------------------------------------
 GateEnable_Del = outerPins.createPort("N","GateEnable_Del")
 DrainEnable_Del = outerPins.createPort("W","DrainEnable_Del")
+
+WTA_CTRL_B = outerPins.createPort("E","WTA_CTRL_B",dimension=2)
+WTA_Vg = outerPins.createPort("E","WTA_Vg",dimension=2)
+WTA_Prog = outerPins.createPort("E","WTA_Prog")
 # Pin Connections Delay Lines
 # -------------------------------------------------------------------------------
+for i in range(rows):
+    DelayLine_instances[0][i].V_NW += Input[i]
 #for i in range(2*columns-1):
     #GateSwitches_Del.RUN_IN[i] += VGRUN
 GR_Del.VGRUN += VGRUN
+GR_Half.AVDD += AVDD
 GateSwitches_Del.Vgsel += VGPROG
 GateSwitches_Del.PROG += PROG
 GateSwitches_Del.RUN += RUN
@@ -163,13 +203,52 @@ for i in range(drainBits_Del):
 #DrainDecoder_Del.IN += DrainB_Del
 DrainDecoder_Del.ENABLE += DrainEnable_Del
 
-offset = 50000
+GateSwitches_Del.VDD[18] += AVDD
+GateSwitches_Del.VDD[19] += GateSwitches_Del.VDD[18]
+GateSwitches_Del.CTRL_B[18] += WTA_CTRL_B[0]
+GateSwitches_Del.CTRL_B[19] += WTA_CTRL_B[1]
+GateSwitches_Del.Vg[18] += WTA_Vg[0]
+GateSwitches_Del.Vg[19] += WTA_Vg[1]
+
+Scanner[0].Out += Scan_out
+Scanner[0].Din += Din
+Scanner[0].CLK += CLK
+Scanner[0].RSTBar += RSTBar
+Scanner[0].VDD += AVDD
+Scanner[0].GND += GND_S
+
+count = 0
+for i in range(columns):
+    if i == 0:
+        for j in range(rows):
+            DelayLine_instances[i][j].V_NW += OUTS[count]
+            count += 1
+        for j in range(rows):
+            DelayLine_instances[i][j].V_NE += OUTS[count]
+            count += 1
+    else:
+        for j in range(rows):
+            DelayLine_instances[i][j].V_NE += OUTS[count]
+            count += 1
+
+with open('./ashes_fg/asic/qrouter_default.json') as file:
+    qparams = json.load(file)
+
+qparams["passes"] = 50
+qparams["via"] = 10
+qparams["jog"] = 35
+qparams["conflict"] = 40
+qparams["stage1"] = "mask auto force"
+qparams["stage2"] = "mask bbox force effort 500"
+qparams["stage3"] = "mask bbox force effort 500"
+
+offset = 120000
 mult = 1.2
 #mult = 4.4
 #design_limits = [7e5, 8e5]
-design_limits = [12e6, 12e6]
+design_limits = [5e6, 5e6]
 #location_islands = ((200000,offset),(240000,(22000*10)+10000+offset),(100000,320000),(mult*X_val,320000))
-location_islands = ((100000,offset),(540000,(22000*32)+90000+offset),(540000,866001))
+location_islands = ((70000,offset),(490000,(22000*rows)+90000+offset),(490000,1112001), (689180,1112001), (500000, 20000))
 #design_limits = [1e6, 3e6]
 #location_islands = ((50000,25000),(240000,22000*130))
 compile_asic(Top,process="TSMC350nm",fileName="Delaylines",p_and_r = True,design_limits = design_limits, location_islands = location_islands,drainSpaceIdx=0,drainSpace = 0,gateSpaceIdx=0,gateSpace=10)
