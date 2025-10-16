14ba7177de9bc17c68225437bcab0ca6dadf0601
prenziv <prenziv@gmail.com>
2025-10-15 23:44:48 -0400

    Deleted problematic PCH in FNN_Relu_and_Sig

--- FullyCon_NN/verilog_files/FullyCon_NN.v
+++ FullyCon_NN/verilog_files/FullyCon_NN.v
@@ -2,9 +2,9 @@ module TOP(port1);
 
 
 	/* Island 0 */
-	TSMC350nm_4x2_Indirect I__0 (.island_num(0), .row(0), .col(0), .matrix_row(5), .matrix_col(5), .matrix_row(5), .matrix_col(5));
-	FNN_DiodeConn I__1 (.island_num(0), .row(0), .col(5), .matrix_row(5), .matrix_col(1), .matrix_row(5), .matrix_col(1));
-	FNN_Relu_and_Sig I__2 (.island_num(0), .row(0), .col(6), .matrix_row(5), .matrix_col(1), .matrix_row(5), .matrix_col(1));
+	TSMC350nm_4x2_Indirect I__0 (.island_num(0), .row(0), .col(0), .matrix_row(5), .matrix_col(5));
+	FNN_DiodeConn I__1 (.island_num(0), .row(0), .col(5), .matrix_row(5), .matrix_col(1));
+	FNN_Relu_and_Sig I__2 (.island_num(0), .row(0), .col(6), .matrix_row(5), .matrix_col(1));
 
  	/*Programming Mux */ 
 	TSMC350nm_IndirectSwitches switch(.island_num(0), .direction(horizontal), .num(5));
--- example_python/FullyCon_NN.py
+++ example_python/FullyCon_NN.py
@@ -26,21 +26,17 @@ def FullyCon_Layer(circuit,input_size=10,no_of_neurons=10,FNN_Island=None,island
     #################  Defining VMM for the Neurons #################
     VMM_fr_Neurons = lib_new.TSMC350nm_4x2_Indirect(Top,FNN_Island,dim=[(no_of_neurons*2)//4,input_size//2])
     VMM_fr_Neurons.place([0,0])
-    VMM_fr_Neurons.markAbut()
     track_col = input_size//2
 
     #################  Defining Diodeconnected FETs on the TA/Neuron inputs #################
     Diodeconn_fr_Neurons = lib_new.FNN_DiodeConn(Top,FNN_Island,dim=[(no_of_neurons*2)//4,1])
     Diodeconn_fr_Neurons.place([0,track_col])
-    Diodeconn_fr_Neurons.markAbut()
     track_col = track_col + 1
 
     #################  Defining Neurons #################
     Neurons = lib_new.FNN_Relu_and_Sig(Top,FNN_Island,dim=[no_of_neurons//2,1])
     Neurons.place([0,track_col])
-    Neurons.markAbut()
     track_col = track_col + 1
-
 
     #################  Defining GateSwcs, DrainSwcs and Decoders  #################
     GateSwitches = lib_mux.STD_IndirectGateSwitch(circuit,FNN_Island,input_size//2)
@@ -64,9 +60,9 @@ def FullyCon_Layer(circuit,input_size=10,no_of_neurons=10,FNN_Island=None,island
 Top = ac.Circuit()
 FullyCon_Layer(Top,islandLoc=[100,100],debug=True)
 
-location_islands = ((100,100),(0,0))
+location_islands = ((10000,10000),(0,0))
 
 design_limits = [4e7, 4e7]
 
-ac.compile_asic(Top,process="TSMC350nm", fileName="FullyCon_NN", p_and_r = True, route=False, design_limits = design_limits, location_islands = location_islands, drainSpaceIdx=0,drainSpace=0,gateSpaceIdx=0,gateSpace=0)
+ac.compile_asic(Top,process="TSMC350nm", fileName="FullyCon_NN", p_and_r = True, route=False, design_limits = design_limits, location_islands = location_islands)
 
