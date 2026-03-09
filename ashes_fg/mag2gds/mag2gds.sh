d7bee74562620a2c3a8fd34e2f6c05a1fa473b72
Shahir Latif <shahirlatif1@gmail.com>
2026-03-09 14:33:55 -0400

    update deleted files

--- ashes_fg/mag2gds/mag2gds.sh
+++
@@ -1,3 +0,0 @@
-RC="/srv/cadsp/pdks/open_pdks/sky130/sky130A/libs.tech/magic/sky130A.magicrc" 
-magic -rcfile "$RC" -nowindow -noconsole mag2gds.tcl
-
--- ashes_fg/mag2gds/mag2gds.tcl
+++
@@ -1,6 +0,0 @@
-grid 0.05um 0.05um
-snap user
-load "/home/slatif8/Documents/ASHES-Skywater130nm/sky130_cells/IndirectVMM_4x2/IndirectVMM_4x2_ashes.mag"
-gds write test.gds
-quit -noprompt
-
