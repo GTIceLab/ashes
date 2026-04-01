8fba2c6fc3b42f8e54961a070d9ebc5956f89d57
bharmai <mbharathi7@gatech.edu>
2026-03-31 20:09:15 -0400

    trying to fix waveform

--- ashes_fg/class_lib.py
+++ ashes_fg/class_lib.py
@@ -292,9 +292,12 @@ class test:
                  num_instances='1',
                  type='FPAA',
                  board=['3.0', '3.0a'],
-        		):
+                 test_ls='0',
+                 test_cap0_1x_cs='1'):
         self.input = input
         self.num_instances = num_instances
+        self.test_ls = test_ls
+        self.test_cap0_1x_cs = test_cap0_1x_cs
 
 class hhn:
     def __init__(self,
